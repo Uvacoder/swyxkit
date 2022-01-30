50cefc62ee7495f0a485b1bb02539c9bc1b416a1
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-30 18:14:43 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { mdsvex } from 'mdsvex';
 import remarkGithub from 'remark-github';
 import remarkAbbr from 'remark-abbr';
@@ -49,7 +49,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel({
+		adapter: adapter({
 			split: false
 		})
 	}
